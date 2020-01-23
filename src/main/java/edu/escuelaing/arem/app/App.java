32e625201e28c72e20df4bcc02912f1cf1cb22f3
dcifuentesR <daniel.cifuentes-r@mail.escuelaing.edu.co>
2020-01-23 13:31:58 -0500

    lee archivos

--- src/main/java/edu/escuelaing/arem/app/App.java
+++ src/main/java/edu/escuelaing/arem/app/App.java
@@ -1,5 +1,9 @@
 package edu.escuelaing.arem.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import edu.escuelaing.arem.app.*;
 
 /**
@@ -8,9 +12,19 @@ import edu.escuelaing.arem.app.*;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FileNotFoundException
     {
-    	LinkedList l = new LinkedList();
+    	File f = new File("src\\resources\\numbers");
+    	LinkedList<Integer> l = new LinkedList<Integer>();
+    	Scanner scanner = new Scanner(f);
+    	while(scanner.hasNext()) {
+        	int myInt = scanner.nextInt();
+        	l.add(myInt);
+    	}
+
+    	for(int i:l)
+    		System.out.print(i);
+    	scanner.close();
     	
     }
 }
